ef0ddc1e7ce271a4f60215608cd8d448d8440ccd
johnfb64 <jhonn.ballen@gmail.com>
2021-04-16 17:14:27 -0500

    Create IOexamples.sh

---
+++ IOexamples.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: John Ballen
+# Date Created:04-16-2021
+# Description: The scrpt run basic admin commands
+# Date Modified: 04-16-2021
+
+#Take care with the tick key!
+a=`hostname`
+echo My name is $a
+echo
+echo What is your name?
+read b
+echo
+echo Nice to meet you $b
+echo
