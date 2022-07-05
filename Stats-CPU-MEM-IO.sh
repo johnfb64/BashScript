5db146cbcccbef0b8ff3179d6802208d8098d82a
johnfb64 <jhonn.ballen@gmail.com>
2022-07-05 08:10:53 -0500

    script para tomar estadisticas. Se corrige variable de LOG

--- Stats-CPU-MEM-IO.sh
+++ Stats-CPU-MEM-IO.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#####################################################################
-# SCRIPT realizado por John Ballén                                  #
-# Este script toma estadisticas de comportamiento de CPU, Memoria   #
-# e Iostat de discos en servidores Solaris.                         #
-#####################################################################
-
-###########################CONSTANTES################################
+###########################CONSTANTES############################
 fecha=`/bin/date '+%d-%m-%Y,%H:%M'`
 fecha_hoy=`/bin/date '+%d-%m-%Y'`
 
 ###########################VARIABLES############################
-SERVIDOR="xx.xx.xx.xx"
-host="batch01"
-BATCH01_LOG=/root/Estadisticas/batch01/Estadisticas_batch01_$fecha_hoy.txt
-BATCH01_IOSTAT_LOG=/root/Estadisticas/batch01/Iostat_batch01_$fecha_hoy.txt
-
+#cambiar solo estos parametros -----------------------------------------
+SERVIDOR="admin@10.181.0.xx"
+host="servidor"
+LOG_MEM_CPU=/root/Estadisticas/servidor/Estadisticas_servidor_$fecha_hoy.txt
+LOG_IOSTAT_LOG=/root/Estadisticas/servidor/Iostat_servidor_$fecha_hoy.txt
+#------------------------------------------------------------------------
 ###########################PROCESADOR############################
 CPU_CANT=`ssh $SERVIDOR mpstat | wc -l`
 for i in $(seq 2 $CPU_CANT)
@@ -26,7 +21,7 @@ do
 done
 #echo $COUNT
 AVG=$((COUNT / CPU_CANT))
-echo "$host,AVG_CPU,$fecha,$AVG" >> $BATCH01_LOG
+echo "$host,AVG_CPU,$fecha,$AVG" >> $LOG_MEM_CPU
 
 ###########################MEMORIA############################
 MEMORIA=`ssh $SERVIDOR top -b -n 1 | grep Memory | awk '{print $2}' | sed 's/G//g'`
@@ -34,13 +29,16 @@ LIBRE=`ssh $SERVIDOR top -b -n 1 | grep Memory | awk '{print $5}' | sed 's/G//g'
 #echo $MEMORIA
 #echo $LIBRE
 OCUPADO=$((MEMORIA - LIBRE))
-echo "$host,STAT_MEM,$fecha,$MEMORIA,$LIBRE,$OCUPADO" >> $BATCH01_LOG
+echo "$host,STAT_MEM,$fecha,$MEMORIA,$LIBRE,$OCUPADO" >> $LOG_MEM_CPU
 
 ###########################IOSTAT############################
 ZPOOL_CANT=`ssh $SERVIDOR zpool list | wc -l`
 for i in $(seq 2 $ZPOOL_CANT)
 do
-   DISK_ID=`ssh $SERVIDOR zpool list | awk -v fila="$i" 'NR==fila' | awk '{print "zpool status "$1}' | ssh $SERVIDOR | grep c0t | awk '{print "/usr/bin/iostat -xpns 2 1 | grep -w " $1}' | ssh $SERVIDOR | tail -1 | awk '{print "asvc_t "$8","$11}'`
-   echo "$host,IOSTAT,$fecha,$DISK_ID" >> $BATCH01_IOSTAT_LOG
+   DISK_ID=`ssh $SERVIDOR zpool list | awk -v fila="$i" 'NR==fila' | awk '{print "zpool status "$1}' \
+   | ssh $SERVIDOR | grep c0t | awk '{print "/usr/bin/iostat -xpns 2 1 | grep -w " $1}' | \
+   ssh $SERVIDOR | tail -1 | awk '{print "asvc_t "$8","$11}'`
+   echo "$host,IOSTAT,$fecha,$DISK_ID" >> $LOG_IOSTAT_LOG
 done
+
 
