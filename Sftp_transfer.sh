1f846089b886a50e29da0aee731c50280a38848d
johnfb64 <jhonn.ballen@gmail.com>
2022-08-02 07:53:40 -0500

    Se cambia scp por rsync para mayor estabilidad de data

--- Sftp_transfer.sh
+++ Sftp_transfer.sh
@@ -1,34 +1,54 @@
 #!/usr/bin/bash
+
+#####################################################################
+# FUNCION PARA TRANSFERENCIA DE ARCHIVOS DE BATMAN HACIA CARVAJAL   #
+# AUTOR: JOHN BALLEN                                                #
+# FECHA: 29-07-2022                                                 #
+#####################################################################
+
+OrdenesCompra () {
+
+echo "############# MODULO DE ORDENES #################" >> $LOG
 ########## VARIABLES ##########################################
-LOG="poner ruta del log"
+LOG="/home/carvajal/logs/traerbatch01_orders.log"
 DATE=`date '+%d-%m-%Y,%H:%M'`
-HOST="poner usuario@servidor"
-PWD="password aqui"
-ORIGEN="/de donde se halan los archivos"
-ARCHIVO="cuales son los archivos"
-DESTINO="para donde van"
+HOST="USUARIO@SERVIDOR"
+PWD="password_de_servidor"
+ORIGEN="/reportes/CKPR/files/EDICEN"
+FOLDERINSUMO="/home/carvajal/EDICEN"
+ARCHIVO="/home/carvajal/EDICEN/*.test"
+#DESTINO="/ORDERS/IN"
+DESTINO="/"
+FORMAT="test"
+BCK="/reportes/CKPR/files/bckedicen/test_new_Script"
 
 ############ TRAE ARCHIVOS DE BATCH ############################
-cd $ORIGEN
-scp -pr usuario@servidor:$ORIGEN/*.txt .
+rsync -avzh --remove-source-files --progress psoft85@10.181.0.77:$ORIGEN/*.$FORMAT $FOLDERINSUMO
+
 if [ $? = 0 ]
-   then
-   echo "Archivos traidos correctamente" >> $LOG
-else
-   echo "Archivos no transmitidos" >> $LOG
-fi
+then
+echo "$DATE - Archivos traidos correctamente" >> $LOG
 
 ########### ENVIA ARCHIVOS A SFTP ##############################
 sshpass -p $PWD sftp -F /dev/null -o PreferredAuthentications=password $HOST<<EOF
 cd $DESTINO
+lcd $FOLDERINSUMO
 put $ARCHIVO
 bye
 EOF
 
-######### LIMPIEZA DE CARPETA ###############################
+######## BACKUP ARCHIVOS Y LIMPIEZA ############################
+scp -pr $FOLDERINSUMO/*.$FORMAT psoft85@10.181.0.77:$BCK
+if [ $? = 0 ]
+then
 sleep 3
-cd /home/donde_qudan_los archivos
-rm -f /home/donde_qudan_los archivos/*.edi
+cd $FOLDERINSUMO
+rm -f $ARCHIVO
+else
+echo "$DATE - Archivos no transmitidos hacia backup" >> $LOG
+fi
+else "$DATE - Archivos no transmitidos hacia servidor" >> $LOG
+fi
+}
 
-####################### REALIZA BACKUP #######################
 
